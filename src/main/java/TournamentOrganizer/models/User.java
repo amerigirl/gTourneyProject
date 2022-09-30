cfcef5fe7da86f3c5e8db55be8c07528871ea159
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-09-29 22:07:20 -0500

    finish tweaking the event controller

--- src/main/java/TournamentOrganizer/models/User.java
+++ src/main/java/TournamentOrganizer/models/User.java
@@ -17,14 +17,18 @@ public class User extends AbstractEntity {
     //constructors
     public User(){}
 
+    //encodes the password using encoder
     public User(String username, String password) {
         this.username = username;
         this.pwHash = encoder.encode(password);
     }
 
+    //validates password and pwHash as the same
     public boolean isMatchingPassword(String password) {
         return encoder.matches(password, pwHash);
     }
+
+
     //getter
     public String getUsername() {
         return username;
